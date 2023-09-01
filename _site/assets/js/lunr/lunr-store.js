a8d00ac4f248fda693f9a6052fa39dc958cb8836
felixmattsson <felix_mattsson@hotmail.com>
2023-09-01 23:30:13 +0200

    add something

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Why You Should Invest Your Money",
+        "excerpt":"Do you ever think about investing money? Since a few years back, I have been convinced that investing money is a good thing to do and that i want to do more of, and here I’ll put forth some of my reasons for why. The topic of investments is based...","categories": [],
+        "tags": [],
+        "url": "/why-you-should-invest/",
+        "teaser": "/assets/images/pietro-de-grandi-6U4wogjLArk-unsplash.jpg"
+      },{
+        "title": "7 Actions for Consuming More Books (to Learn and Improve Your Life)",
+        "excerpt":"Books are a great way of learning and improving your life, but it is all too easy to down-prioritize them to spend time on more urgent, but less impactful things. In this video, you will find my 7 actions for how to be more intentional with consuming books and making...","categories": [],
+        "tags": [],
+        "url": "/7-actions-for-consuming-more-books/",
+        "teaser": "/assets/images/pietro-de-grandi-6U4wogjLArk-unsplash.jpg"
+      }]
